3c127006e78bafbc83f3771a21fa896e54616d69
Jonathan Whiting <jonathan@ignika.com>
2015-12-12 20:03:30 +0000

    Rendering some inner text

--- src/menu/menu.c
+++ src/menu/menu.c
@@ -13,26 +13,32 @@ space_menu space_menu_update(space_menu menu, space_game game)
 	return menu;
 }
 
-void draw_string(const char* string, whitgl_ivec pos, whitgl_sprite sprite, whitgl_bool centered)
+void draw_string(const char* string, whitgl_ivec pos, whitgl_int max_width, whitgl_sprite sprite, whitgl_bool centered)
 {
 	whitgl_int len = strlen(string);
 	if(centered)
 		pos.x -= (sprite.size.x*len)/2;
+	whitgl_ivec draw_pos = pos;
 	while(*string)
 	{
 		if(*string >= 'a' && *string <= 'z')
 		{
 			int index = *string-'a';
 			whitgl_ivec frame = {index%6, index/6};
-			whitgl_sys_draw_sprite(sprite, frame, pos);
+			whitgl_sys_draw_sprite(sprite, frame, draw_pos);
 		}
 		if(*string >= '0' && *string <= '9')
 		{
 			int index = *string-'0'+26;
 			whitgl_ivec frame = {index%6, index/6};
-			whitgl_sys_draw_sprite(sprite, frame, pos);
+			whitgl_sys_draw_sprite(sprite, frame, draw_pos);
 		}
-		pos.x += sprite.size.x;
+		draw_pos.x += sprite.size.x;
+		if(draw_pos.x - pos.x > max_width)
+		{
+			draw_pos.x = pos.x;
+			draw_pos.y += sprite.size.y;
+		}
 		string++;
 	}
 }
@@ -58,6 +64,12 @@ void space_menu_draw(space_menu menu, whitgl_ivec screen_size)
 	{
 		whitgl_sprite big_font = {IMAGE_FONT, {36,0}, {12,12}};
 		whitgl_ivec title_pos = {(title_box.a.x+title_box.b.x)/2, title_box.a.y};
-		draw_string("diso", title_pos, big_font, true);
+		draw_string("diso", title_pos, box_size.x, big_font, true);
+	}
+	if(box_size.y == 128)
+	{
+		whitgl_sprite little_font = {IMAGE_FONT, {0,0}, {6,6}};
+		whitgl_ivec text_pos = {title_box.a.x+2, title_box.a.y+2+12};
+		draw_string("this is a lot of nonsense text isn't it", text_pos, box_size.x-12, little_font, false);
 	}
 }
