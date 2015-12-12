ac759082d301fc8e8d75ccb0607190b311bf4a88
Jonathan Whiting <jonathan@ignika.com>
2015-12-12 09:40:28 +0000

    Added missing capture files

---
+++ src/capture.c
@@ -0,0 +1,31 @@
+#include "capture.h"
+
+#include <stdio.h>
+
+#include <whitgl/input.h>
+#include <whitgl/logging.h>
+#include <whitgl/sys.h>
+
+capture_info capture_info_update(capture_info capture, whitgl_bool capturing)
+{
+	if(capturing)
+		capture.capturing = !capture.capturing;
+	if(!capture.capturing)
+	{
+		capture.frame = 0;
+		capture.timer = 0;
+		return capture;
+	}
+	if(capture.frame == 0)
+		capture.n++;
+	capture.timer--;
+	if(capture.timer > 0)
+		return capture;
+	capture.timer = 2;
+	char buffer[256];
+	snprintf(buffer, sizeof(buffer), "../../gifsrc/seq%04d_%04d.png", (int)capture.n, (int)capture.frame);
+	WHITGL_LOG("capturing %s", buffer);
+	whitgl_sys_capture_frame(buffer);
+	capture.frame++;
+	return capture;
+}
---
+++ src/capture.h
@@ -0,0 +1,17 @@
+#ifndef CAPTURE_H_
+#define CAPTURE_H_
+
+#include <whitgl/math.h>
+
+typedef struct
+{
+	whitgl_bool capturing;
+	whitgl_int timer;
+	whitgl_int frame;
+	whitgl_int n;
+} capture_info;
+static const capture_info capture_info_zero = {false, 0, 0, -1};
+
+capture_info capture_info_update(capture_info capture, whitgl_bool capturing);
+
+#endif // CAPTURE_H_
