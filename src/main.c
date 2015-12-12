89edcfb80dd6552dc4417f835736c01282d4d30c
Jonathan Whiting <jonathan@ignika.com>
2015-12-12 08:55:35 +0000

    Store and draw super simple ship sprite

--- src/main.c
+++ src/main.c
@@ -8,6 +8,42 @@
 #include <whitgl/sys.h>
 #include <whitgl/timer.h>
 
+#define MAX_POINTS (32)
+#define MAX_LINES (32)
+typedef struct
+{
+	whitgl_fvec points[MAX_POINTS];
+	whitgl_ivec lines[MAX_LINES];
+	whitgl_int num_points;
+	whitgl_int num_lines;
+} space_sprite;
+
+space_sprite ship_sprite = {
+	{{0.5,0},{1.0,1.0},{0.0,1.0}},
+	{{0,1},{1,2},{0,2}},
+	3,
+	3,
+};
+
+void draw_sprite(space_sprite sprite)
+{
+	whitgl_int i;
+	for(i=0; i<sprite.num_lines; i++)
+	{
+		whitgl_fvec a = whitgl_fvec_scale_val(sprite.points[sprite.lines[i].x], 64);
+		whitgl_fvec b = whitgl_fvec_scale_val(sprite.points[sprite.lines[i].y], 64);
+		whitgl_int j;
+		for(j=0; j<64; j++)
+		{
+			whitgl_faabb box;
+			box.a = whitgl_fvec_interpolate(a, b, j/64.0);
+			box.b = whitgl_fvec_add(box.a, whitgl_fvec_val(1));
+			whitgl_sys_color col = {0x00,0xff,0x00,0xff};
+			whitgl_sys_draw_iaabb(whitgl_faabb_to_iaabb(box), col);
+		}
+	}
+}
+
 int main()
 {
 	WHITGL_LOG("Starting main.");
@@ -41,6 +77,7 @@ int main()
 				running = false;
 		}
 		whitgl_sys_draw_init();
+		draw_sprite(ship_sprite);
 		whitgl_sys_draw_finish();
 	}
 
