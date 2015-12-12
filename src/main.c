fc0673b7d36dfa0ccc097f6ab4ac7b0dcc8fb61c
Jonathan Whiting <jonathan@ignika.com>
2015-12-12 09:11:34 +0000

    Some initial camera support

--- src/main.c
+++ src/main.c
@@ -25,14 +25,27 @@ space_sprite ship_sprite = {
 	6,
 };
 
-void draw_sprite(space_sprite sprite)
+typedef struct
+{
+	whitgl_fvec pos;
+	whitgl_float scale;
+} space_camera;
+
+whitgl_fvec space_camera_point(whitgl_fvec p, space_camera cam)
+{
+	p = whitgl_fvec_add(p, cam.pos);
+	p = whitgl_fvec_scale_val(p, cam.scale);
+	return p;
+}
+
+void draw_sprite(space_sprite sprite, space_camera camera)
 {
 	whitgl_int i;
 	for(i=0; i<sprite.num_lines; i++)
 	{
 		whitgl_faabb line;
-		line.a = whitgl_fvec_scale_val(sprite.points[sprite.lines[i].x], 64);
-		line.b = whitgl_fvec_scale_val(sprite.points[sprite.lines[i].y], 64);
+		line.a = space_camera_point(sprite.points[sprite.lines[i].x], camera);
+		line.b = space_camera_point(sprite.points[sprite.lines[i].y], camera);
 		whitgl_sys_color col = {0x00,0xff,0x00,0xff};
 		whitgl_sys_draw_line(whitgl_faabb_to_iaabb(line), col);
 	}
@@ -71,7 +84,8 @@ int main()
 				running = false;
 		}
 		whitgl_sys_draw_init();
-		draw_sprite(ship_sprite);
+		space_camera camera = {{1.5,1}, 64};
+		draw_sprite(ship_sprite, camera);
 		whitgl_sys_draw_finish();
 	}
 
