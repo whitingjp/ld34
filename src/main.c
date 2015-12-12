bf64eda695a1453a09971d3909381dcbc3294ff8
Jonathan Whiting <jonathan@ignika.com>
2015-12-12 09:40:38 +0000

    Simple movement

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #include <whitgl/sound.h>
 #include <whitgl/sys.h>
 #include <whitgl/timer.h>
+#include <capture.h>
 
 #define MAX_POINTS (32)
 #define MAX_LINES (32)
@@ -38,7 +39,7 @@ whitgl_fvec space_camera_point(whitgl_fvec p, space_camera cam)
 	return p;
 }
 
-void draw_sprite(space_sprite sprite, whitgl_float angle, space_camera camera)
+void draw_sprite(space_sprite sprite, whitgl_fvec position, whitgl_float angle, space_camera camera)
 {
 	whitgl_int i;
 	for(i=0; i<sprite.num_lines; i++)
@@ -47,6 +48,10 @@ void draw_sprite(space_sprite sprite, whitgl_float angle, space_camera camera)
 		whitgl_fvec b = sprite.points[sprite.lines[i].y];
 		a = whitgl_rotate_point_around_point(a, whitgl_fvec_val(0.5), angle);
 		b = whitgl_rotate_point_around_point(b, whitgl_fvec_val(0.5), angle);
+		a = whitgl_fvec_add(a, position);
+		b = whitgl_fvec_add(b, position);
+		a = whitgl_fvec_sub(a, whitgl_fvec_val(0.5));
+		b = whitgl_fvec_sub(b, whitgl_fvec_val(0.5));
 		whitgl_faabb line;
 		line.a = space_camera_point(a, camera);
 		line.b = space_camera_point(b, camera);
@@ -60,10 +65,8 @@ int main()
 	WHITGL_LOG("Starting main.");
 
 	whitgl_sys_setup setup = whitgl_sys_setup_zero;
-	//setup.size.x = 280;
-	//setup.size.y = 180;
-	setup.size.x = 128;
-	setup.size.y = 128;
+	setup.size.x = 280;
+	setup.size.y = 180;
 	setup.pixel_size = 3;
 	setup.name = "space";
 
@@ -75,7 +78,11 @@ int main()
 
 	whitgl_timer_init();
 
+	capture_info capture = capture_info_zero;
+	bool capturing = false;
 	whitgl_float angle;
+	whitgl_fvec position = {2,2};
+	whitgl_fvec speed = whitgl_fvec_zero;
 	bool running = true;
 	while(running)
 	{
@@ -89,12 +96,28 @@ int main()
 				running = false;
 			if(whitgl_sys_should_close())
 				running = false;
-			angle = whitgl_fwrap(angle+0.05, 0, whitgl_pi*2);
+			whitgl_bool l = whitgl_input_down(WHITGL_INPUT_LEFT);
+			whitgl_bool r = whitgl_input_down(WHITGL_INPUT_RIGHT);
+			if(l&&r)
+			{
+				whitgl_fvec impulse = {0,-0.005};
+				impulse = whitgl_rotate_point_around_point(impulse, whitgl_fvec_zero, angle);
+				speed = whitgl_fvec_add(speed, impulse);
+			}
+			if(l)
+				angle = whitgl_fwrap(angle-0.1, 0, whitgl_pi*2);
+			if(r)
+				angle = whitgl_fwrap(angle+0.1, 0, whitgl_pi*2);
+			position = whitgl_fvec_add(position, speed);
 		}
 		whitgl_sys_draw_init();
-		space_camera camera = {{0.5,0.5}, 64};
-		draw_sprite(ship_sprite, angle, camera);
+		space_camera camera = {{0.0,0.0}, 32};
+		whitgl_iaabb screen_rect = {whitgl_ivec_zero, setup.size};
+		whitgl_sys_color blank_col = {0x00, 0x00, 0x00, 0xff};
+		whitgl_sys_draw_iaabb(screen_rect, blank_col);
+		draw_sprite(ship_sprite, position, angle, camera);
 		whitgl_sys_draw_finish();
+		capture = capture_info_update(capture, capturing);
 	}
 
 	whitgl_input_shutdown();
